cfd9929bb9bd2020aefd694bfef4e0968a1bacaf
Monika <monika_streljajeva@hotmail.com>
2022-01-10 17:12:11 +0100

    add modal text

--- src/compontents/modal/index.js
+++ src/compontents/modal/index.js
@@ -26,48 +26,72 @@ const Modal = () => {
         </h2>
         <Slider {...settings} ref={sliderRef}>
           <div className="w-10/12 mx-auto mb-10">
-            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-72 md:h-72 font-poppins">
+            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-64 md:h-64 font-poppins">
               {" "}
               <h3 className="w-full p-4 pt-2 pb-0 font-bold text-md">
                 Wat is de aanname tool?
               </h3>
-              <p className="w-full p-4 pt-2 pb-0 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
+              <p className="w-full p-4 pt-2 pb-0 pr-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
+                De aanname tool is een conversational tool
+                <br></br>
+                <br></br>
+                Gezamenlijk haal je aannames naar boven over een project en
+                maakt deze naar bespreekbaar
+              </p>
+            </div>
+          </div>
+          <div className="w-10/12 mx-auto mb-10">
+            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-64 md:h-64 font-poppins">
+              {" "}
+              <h3 className="w-full p-4 pt-2 pb-0 font-bold text-md">
+                Wat zijn aannames??
+              </h3>
+              <p className="w-full p-4 pt-2 pb-0 pr-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
                 Een aanname is iets wat je denkt of vind zonder het zeker te
                 weten.
                 <br></br>
                 <br></br>
-                Met de aanname tool ga je gezamenlijk de aannames naar boven
-                halen en bespreekbaar maken
+                Belangrijk is dat ze niet vast staan
               </p>
             </div>
           </div>
           <div className="w-10/12 mx-auto mb-10">
-            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-72 md:h-72 font-poppins">
+            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-64 md:h-64 font-poppins">
               <h3 className="w-full p-4 pt-2 pb-0 font-bold text-md">
                 Aannames opstellen
               </h3>
-              <p className="w-full p-4 pt-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
-                Korte uitleg over feature
+              <p className="w-full p-4 pt-2 pr-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
+                In een korte tijd ga je proberen zo veel mogelijk aannames op te
+                stellen over het te bespreken project
+                <br></br>
+                <br></br>
+                Het maakt niet uit of deze goed of fout zijn
               </p>
             </div>
           </div>
           <div className="w-10/12 mx-auto mb-10">
-            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-72 md:h-72 font-poppins">
+            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-64 md:h-64 font-poppins">
               <h3 className="w-full p-4 pt-2 pb-0 font-bold text-md">
                 Aannames bekritiseren
               </h3>
               <p className="w-full p-4 pt-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
-                Korte uitleg over feature
+                Doormiddel van kritische vragen, ga je met elkaar de aannames
+                bespreken
+                <br></br>
+                <br></br>
+                Door het te bespreken kom je op nieuwe inzichten
               </p>
             </div>
           </div>
           <div className="w-10/12 mx-auto mb-10">
-            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-72 md:h-72 font-poppins">
+            <div className="w-56 h-auto p-2 m-2 mx-auto mb-12 font-medium text-black bg-white border-2 border-black rounded-md box-shadow md:w-64 md:h-64 font-poppins">
               <h3 className="w-full p-4 pt-2 pb-0 font-bold text-md">
                 Reflecteren op aannames
               </h3>
-              <p className="w-full p-4 pt-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
-                Korte uitleg over feature
+              <p className="w-full p-4 pt-2 pr-2 placeholder-black bg-white rounded-md resize-none font-xss h-28 hover:bg-opacity-25 focus:outline-none">
+                Reflecteer op hoe de aannames binnen het project vallen.
+                <br></br> <br></br>
+                En kom op nieuwe inzichten over de oplossing
               </p>
             </div>
           </div>
